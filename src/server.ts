1cb05263b1d3921840cff87761faaef619eb4c39
Aurino Geraldo <aurinoaj@gmail.com>
2023-06-29 14:17:50 -0300

    Install and configure dotenv

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*')
-  return test
+  const transaction = await knex('transactions').select('*')
+
+  return transaction
 })
 
 app
