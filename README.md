c36558561f497641d585db85e963b20a80387a07
Aurino Geraldo <aurinoaj@gmail.com>
2023-06-29 19:25:28 -0300

    Create main routes for application

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## Requisitos funcionais
 
-- [ ] O usuario deve poder criar uma nova transação;
+- [x] O usuario deve poder criar uma nova transação;
 - [ ] O usuario deve poder obter um resumo da sua conta;
-- [ ] O usuario deve poder listar todas transações que já ocorreram;
-- [ ] O usuario deve poder visualizar uma transação única;
+- [x] O usuario deve poder listar todas transações que já ocorreram;
+- [x] O usuario deve poder visualizar uma transação única;
 
 ## Requisitos de negocio
 
-- [ ] A transação pode ser do tipo crédito que somará ao valor total, ou débito que subtrairá;
+- [x] A transação pode ser do tipo crédito que somará ao valor total, ou débito que subtrairá;
 - [ ] Deve ser possivel identificarmos o usuário entre as requisições;
 - [ ] O usuário só pode visualizar transações o qual ele criou;
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,33 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async (_, reply) => {
+    const transactions = await knex('transactions').select()
+
+    return reply.status(200).send({ transactions })
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return reply.status(200).send({ transaction })
+  })
+
+  app.get('/summary', async (_, reply) => {
+    const summary = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+
+    return reply.status(200).send({ summary })
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
