eb086a341e4478c6b4db6beff432d91da1592f75
Aurino Geraldo <aurinoaj@gmail.com>
2023-06-29 11:56:50 -0300

    config knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const test = await knex('sqlite_schema').select('*')
+  return test
 })
 
 app
