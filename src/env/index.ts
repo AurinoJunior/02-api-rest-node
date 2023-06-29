54606839086d3157a30234279c4427f0ddb41f31
Aurino Geraldo <aurinoaj@gmail.com>
2023-06-29 14:38:43 -0300

    Install and config zod

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.string().default('3333'),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('🚨 Invalid enviroment variables', _env.error.format())
+  throw new Error()
+}
+
+export const env = _env.data
