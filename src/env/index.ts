e3f644414b96411d0596d637ceb3152c4feed9c4
Aurino Geraldo <aurinoaj@gmail.com>
2023-07-03 16:12:17 -0300

    Create test for list, summary and list one transaction

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,13 @@
+import { config } from 'dotenv'
 import 'dotenv/config'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  console.log(process.env.NODE_ENV)
+  config({ path: '.env.test', override: true })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,21 +1,114 @@
-import { test, afterAll, beforeAll, expect } from 'vitest'
+import { test, afterAll, beforeAll, expect, describe, beforeEach } from 'vitest'
 import supertest from 'supertest'
+
 import { app } from '../src/app'
+import { execSync } from 'child_process'
 
-beforeAll(async () => {
-  await app.ready()
-})
-
-afterAll(async () => {
-  await app.close()
-})
-
-test('user can create a new transaction', async () => {
-  const response = await supertest(app.server).post('/transactions').send({
-    title: 'New transaction',
-    amount: 5000,
-    type: 'credit',
+describe('/transactions', () => {
+  beforeAll(async () => {
+    await app.ready()
   })
 
-  expect(response.status).toEqual(201)
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('yarn knex migrate:rollback --all')
+    execSync('yarn knex migrate:latest')
+  })
+
+  test('should be able to create a new transaction', async () => {
+    const response = await supertest(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.status).toEqual(201)
+  })
+
+  test('should be able to list all transaction', async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await supertest(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listTransactionsResponse.body).toEqual({
+      transactions: [
+        expect.objectContaining({
+          title: 'New transaction',
+          amount: 5000,
+        }),
+      ],
+    })
+  })
+
+  test('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await supertest(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await supertest(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+
+    expect(getTransactionResponse.body).toEqual({
+      transaction: expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    })
+  })
+
+  test('should be able to get the summary', async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    // create second transaciton
+    await supertest(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'New transaction',
+        amount: -2000,
+        type: 'credit',
+      })
+
+    const summaryResponse = await supertest(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
 })
